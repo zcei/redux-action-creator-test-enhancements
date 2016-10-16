fbdd108f365b119581544f5d8c2e2933fc3d1db6
Stephan Schneider <stephanschndr@gmail.com>
2016-10-16 20:33:45 +0200

    clean up proposed changes

--- next.js
+++ next.js
@@ -3,13 +3,13 @@ const tape = process.argv[1].includes('tape')
 const { TEST_ACTION, ARRAY_ACTION, testAction, arrayAction } = require('./test-action')
 const test = tape ? require('tape') : require('ava')
 
-const actionTestAsArray = (actionCreator, data, expected, description) => t => {
+const asArray = (actionCreator, data, expected, description) => t => {
   const args = [].concat(data)
   t.deepEqual(actionCreator(...args), expected, description)
   tape && t.end()
 }
 
-const actionTestMultipleParams = (actionCreator, ...params) => {
+const multipleParams = (actionCreator, ...params) => {
   const length = params.length
   const hasDescription = typeof params[length - 1] === 'string'
   const description = hasDescription ? params[length - 1] : ''
@@ -23,47 +23,38 @@ const actionTestMultipleParams = (actionCreator, ...params) => {
   }
 }
 
-const actionTests = {
-  current: {
-
-  },
-  next: {
-    multipleParams: actionTestMultipleParams,
-    asArray: actionTestAsArray
-  }
-}
-
 // This is how it should work in my opinion
 
-// Allow array as data
-test('asArray with single param', actionTests.next.asArray(
+// Either with `data` being an Array
+test('asArray with single param', asArray(
   testAction,
   'Alice',
   { type: TEST_ACTION, name: 'Alice', age: 42 },
   'testAction(\'Alice\') should have 42 as default age'
 ))
 
-test('asArray with multiple params', actionTests.next.asArray(
+test('asArray with multiple params', asArray(
   testAction,
   ['Bob', 35],
   { type: TEST_ACTION, name: 'Bob', age: 35 },
   'Age should be 35'
 ))
-test('asArray with single array param', actionTests.next.asArray(
+test('asArray with single array param', asArray(
   arrayAction,
   [[0, 1, 2, 3]],
   { type: ARRAY_ACTION, list: [0, 1, 2, 3] },
   'List should be array'
 ))
 
-test('multipleParams with single param', actionTests.next.multipleParams(
+// Or with mutiple parameters accepted
+test('multipleParams with single param', multipleParams(
   testAction,
   'Alice',
   { type: TEST_ACTION, name: 'Alice', age: 42 },
   'testAction(\'Alice\') should have 42 as default age'
 ))
 
-test('multipleParams with multiple params', actionTests.next.multipleParams(
+test('multipleParams with multiple params', multipleParams(
   testAction,
   'Bob',
   35,
@@ -71,7 +62,7 @@ test('multipleParams with multiple params', actionTests.next.multipleParams(
   'Age should be 35'
 ))
 
-test('multipleParams with single array param', actionTests.next.multipleParams(
+test('multipleParams with single array param', multipleParams(
   arrayAction,
   [0, 1, 2, 3],
   { type: ARRAY_ACTION, list: [0, 1, 2, 3] },
